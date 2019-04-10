ef2111f22b5b998b3342f54e20d48b94349b7e6d
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2019-04-10 22:06:04 +0200

    add error handling

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 
-fn draw_linear_bezier_curve(
+fn draw_linear_bezier_curve (
     canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
-    p0: &sdl2::rect::Point,
-    p1: &sdl2::rect::Point,
-) {
+    p0: sdl2::rect::Point,
+    p1: sdl2::rect::Point,
+) -> Result<(), String> {
     canvas.set_draw_color(Color::RGB(0, 255, 0));
 
     let mut t: f64 = 0.0;
@@ -19,13 +19,14 @@ fn draw_linear_bezier_curve(
 
         let x = f64::from(p0.x) * (1.0 - t) + f64::from(p1.x) * t;
         let y = f64::from(p0.y) * (1.0 - t) + f64::from(p1.y) * t;
-        canvas.draw_point(sdl2::rect::Point::new(x as i32, y as i32));
+        canvas.draw_point(sdl2::rect::Point::new(x as i32, y as i32))?;
 
         t += 0.0001;
     }
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
@@ -42,7 +43,7 @@ fn main() {
     let p0 = sdl2::rect::Point::new(200, 100);
     let p1 = sdl2::rect::Point::new(250, 300);
 
-    draw_linear_bezier_curve(&mut canvas, &p0, &p1);
+    draw_linear_bezier_curve(&mut canvas, p0, p1)?;
 
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -59,4 +60,6 @@ fn main() {
             }
         }
     }
+
+    Ok(())
 }
