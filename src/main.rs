e1a1325c8e13978851b1948152e35aa93ba6ca4d
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2019-04-13 20:52:46 +0200

    implement ability to move control points with mouse

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 extern crate sdl2;
 
 use sdl2::event::Event;
+use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::gfx::primitives::DrawRenderer;
+
+fn draw_points(
+    canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+    points: &[Point],
+) -> Result<(), String> {
+    for point in points {
+        point.draw(canvas)?;
+    }
+    Ok(())
+}
 
 fn draw_linear_bezier_curve(
     canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
-    p0: sdl2::rect::Point,
-    p1: sdl2::rect::Point,
+    p0: &Point,
+    p1: &Point,
 ) -> Result<(), String> {
     canvas.set_draw_color(Color::RGB(255, 0, 0));
 
@@ -18,8 +28,8 @@ fn draw_linear_bezier_curve(
             break;
         }
 
-        let x = (1.0 - t) * f64::from(p0.x) + t * f64::from(p1.x);
-        let y = (1.0 - t) * f64::from(p0.y) + t * f64::from(p1.y);
+        let x = (1.0 - t) * f64::from(p0.position.x) + t * f64::from(p1.position.x);
+        let y = (1.0 - t) * f64::from(p0.position.y) + t * f64::from(p1.position.y);
 
         canvas.draw_point(sdl2::rect::Point::new(x as i32, y as i32))?;
 
@@ -30,9 +40,9 @@ fn draw_linear_bezier_curve(
 
 fn draw_quadratic_bezier_curve(
     canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
-    p0: sdl2::rect::Point,
-    p1: sdl2::rect::Point,
-    p2: sdl2::rect::Point,
+    p0: &Point,
+    p1: &Point,
+    p2: &Point,
 ) -> Result<(), String> {
     canvas.set_draw_color(Color::RGB(0, 255, 0));
 
@@ -42,12 +52,12 @@ fn draw_quadratic_bezier_curve(
             break;
         }
 
-        let x = (1.0 - t).powi(2) * f64::from(p0.x)
-            + 2.0 * (1.0 - t) * t * f64::from(p1.x)
-            + t.powi(2) * f64::from(p2.x);
-        let y = (1.0 - t).powi(2) * f64::from(p0.y)
-            + 2.0 * (1.0 - t) * t * f64::from(p1.y)
-            + t.powi(2) * f64::from(p2.y);
+        let x = (1.0 - t).powi(2) * f64::from(p0.position.x)
+            + 2.0 * (1.0 - t) * t * f64::from(p1.position.x)
+            + t.powi(2) * f64::from(p2.position.x);
+        let y = (1.0 - t).powi(2) * f64::from(p0.position.y)
+            + 2.0 * (1.0 - t) * t * f64::from(p1.position.y)
+            + t.powi(2) * f64::from(p2.position.y);
 
         canvas.draw_point(sdl2::rect::Point::new(x as i32, y as i32))?;
 
@@ -58,10 +68,10 @@ fn draw_quadratic_bezier_curve(
 
 fn draw_cubic_bezier_curve(
     canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
-    p0: sdl2::rect::Point,
-    p1: sdl2::rect::Point,
-    p2: sdl2::rect::Point,
-    p3: sdl2::rect::Point,
+    p0: &Point,
+    p1: &Point,
+    p2: &Point,
+    p3: &Point,
 ) -> Result<(), String> {
     canvas.set_draw_color(Color::RGB(0, 0, 255));
 
@@ -71,14 +81,14 @@ fn draw_cubic_bezier_curve(
             break;
         }
 
-        let x = (1.0 - t).powi(3) * f64::from(p0.x)
-            + 3.0 * (1.0 - t).powi(2) * t * f64::from(p1.x)
-            + 3.0 * (1.0 - t) * t.powi(2) * f64::from(p2.x)
-            + t.powi(3) * f64::from(p3.x);
-        let y = (1.0 - t).powi(3) * f64::from(p0.y)
-            + 3.0 * (1.0 - t).powi(2) * t * f64::from(p1.y)
-            + 3.0 * (1.0 - t) * t.powi(2) * f64::from(p2.y)
-            + t.powi(3) * f64::from(p3.y);
+        let x = (1.0 - t).powi(3) * f64::from(p0.position.x)
+            + 3.0 * (1.0 - t).powi(2) * t * f64::from(p1.position.x)
+            + 3.0 * (1.0 - t) * t.powi(2) * f64::from(p2.position.x)
+            + t.powi(3) * f64::from(p3.position.x);
+        let y = (1.0 - t).powi(3) * f64::from(p0.position.y)
+            + 3.0 * (1.0 - t).powi(2) * t * f64::from(p1.position.y)
+            + 3.0 * (1.0 - t) * t.powi(2) * f64::from(p2.position.y)
+            + t.powi(3) * f64::from(p3.position.y);
 
         canvas.draw_point(sdl2::rect::Point::new(x as i32, y as i32))?;
 
@@ -87,11 +97,68 @@ fn draw_cubic_bezier_curve(
     Ok(())
 }
 
-fn draw_point(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, point: sdl2::rect::Point) -> Result<(), String> {
-    const RADIUS: i16 = 5;
-    const COLOR: u32 = 0xFF0000FFu32;
-    canvas.filled_circle(point.x as i16, point.y as i16, RADIUS, COLOR)?;
-    Ok(())
+struct Point {
+    position: sdl2::rect::Point,
+    draw_radius: i16,
+    draw_color: sdl2::pixels::Color,
+    selected: bool,
+}
+
+impl Point {
+    fn new(position: sdl2::rect::Point) -> Point {
+        Point {
+            position,
+            draw_radius: 6,
+            draw_color: sdl2::pixels::Color {
+                r: 255,
+                g: 0,
+                b: 0,
+                a: 255,
+            },
+            selected: false,
+        }
+    }
+
+    fn is_selected(&self, x: i32, y: i32) -> bool {
+        let distance =
+            (f64::from((x - self.position.x).pow(2) + (y - self.position.y).pow(2)).sqrt()) as i16;
+        distance <= self.draw_radius
+    }
+
+    fn mark_selected(&mut self) {
+        self.selected = true;
+        self.draw_color = sdl2::pixels::Color {
+            r: 0,
+            g: 255,
+            b: 0,
+            a: 255,
+        };
+    }
+
+    fn mark_unselected(&mut self) {
+        self.selected = false;
+        self.draw_color = sdl2::pixels::Color {
+            r: 255,
+            g: 0,
+            b: 0,
+            a: 255,
+        };
+    }
+
+    fn draw(&self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) -> Result<(), String> {
+        canvas.filled_circle(
+            self.position.x as i16,
+            self.position.y as i16,
+            self.draw_radius,
+            self.draw_color,
+        )?;
+        Ok(())
+    }
+
+    fn move_to(&mut self, x: i32, y: i32) {
+        self.position.x = x;
+        self.position.y = y;
+    }
 }
 
 fn main() -> Result<(), String> {
@@ -102,30 +169,28 @@ fn main() -> Result<(), String> {
         .window("bezier_curves", 1600, 900)
         .build()
         .unwrap();
-
     let mut canvas = window.into_canvas().build().unwrap();
 
-    canvas.clear();
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    let mut points = vec![
+        Point::new(sdl2::rect::Point::new(100, 50)),
+        Point::new(sdl2::rect::Point::new(250, 800)),
+        Point::new(sdl2::rect::Point::new(1300, 200)),
+        Point::new(sdl2::rect::Point::new(1500, 800)),
+    ];
 
-    let p0 = sdl2::rect::Point::new(100, 50);
-    let p1 = sdl2::rect::Point::new(250, 800);
-    let p2 = sdl2::rect::Point::new(1300, 200);
-    let p3 = sdl2::rect::Point::new(1500, 800);
-
-    draw_linear_bezier_curve(&mut canvas, p0, p1)?;
-    draw_quadratic_bezier_curve(&mut canvas, p0, p1, p2)?;
-    draw_cubic_bezier_curve(&mut canvas, p0, p1, p2, p3)?;
-
-    draw_point(&mut canvas, p0)?;
-    draw_point(&mut canvas, p1)?;
-    draw_point(&mut canvas, p2)?;
-    draw_point(&mut canvas, p3)?;
-
-    canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     'main_loop: loop {
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        draw_linear_bezier_curve(&mut canvas, &points[0], &points[1])?;
+        draw_quadratic_bezier_curve(&mut canvas, &points[0], &points[1], &points[2])?;
+        draw_cubic_bezier_curve(&mut canvas, &points[0], &points[1], &points[2], &points[3])?;
+        draw_points(&mut canvas, &points)?;
+
+        canvas.present();
+
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -133,7 +198,23 @@ fn main() -> Result<(), String> {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'main_loop,
-                Event::MouseButtonDown {x, y, ..} => println!("{}, {}", x, y),
+                Event::MouseButtonDown { x, y, .. } => {
+                    for point in points.iter_mut() {
+                        if point.is_selected(x, y) {
+                            point.mark_selected();
+                        }
+                    }
+                }
+                Event::MouseButtonUp { .. } => {
+                    for point in points.iter_mut().filter(|p| p.selected) {
+                        point.mark_unselected();
+                    }
+                }
+                Event::MouseMotion { x, y, .. } => {
+                    for point in points.iter_mut().filter(|p| p.selected) {
+                        point.move_to(x, y);
+                    }
+                }
                 _ => {}
             }
         }
